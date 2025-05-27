12c1496312a351789980dba190a31a60dc5b726e
Ross Wilson <ross.wilson@homeflow.co.uk>
2025-05-27 18:28:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tarin UI
+
+Accompanying UI application for [tarin](https://github.com/rosswilson2306/tarin)
+
+## How to Use
+
+
+### Install dependencies
+
+You can use one of them `npm`, `yarn`, `pnpm`, `bun`, Example using `npm`:
+
+```bash
+yarn install
+```
+
+### Run the development server
+
+```bash
+yarn dev
+```
+
+## License
+
+Licensed under the [MIT license]().
---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/heroui-inc/heroui",
+  },
+};
